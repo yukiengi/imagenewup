25001b973cbecf126316a6884c7dcc87a83e3658
yuki <yukiiso0105@icloud.com>
2021-11-28 01:54:07 +0900

    true

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["S3_ACCESS_KEY"],
-      aws_secret_access_key: ENV["S3_SECRET_KEY"],
-      region: ENV["S3_REGION"],
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      region: Rails.application.credentials.dig(:aws, :region),
     }
-    config.fog_directory = ENV["S3_BUCKET"]
-    config.asset_host = "https://dzq55sks3twxy.cloudfront.net"
+    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
   end
 end
